021e4be81bfc7a588ba0cae8308af470a90162cd
Rui Pina <1201568@isep.ipp.pt>
2022-07-17 09:51:47 +0100

    Added integration with an SMS library (Twilio) to send sales details to my phone.

--- backend/src/main/java/app/netlify/ruipina/sales/application/SaleController.java
+++ backend/src/main/java/app/netlify/ruipina/sales/application/SaleController.java
@@ -3,25 +3,31 @@ package app.netlify.ruipina.sales.application;
 
 import app.netlify.ruipina.sales.domain.Sale;
 import app.netlify.ruipina.sales.services.SaleService;
+import app.netlify.ruipina.sales.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
     @Autowired
     private SaleService saleService = new SaleService();
+    @Autowired
+    private SmsService smsService = new SmsService();
+
     @GetMapping
-    public Page<Sale> findSales(@RequestParam(name="minDate", defaultValue = ("")) String minDate,
-                                @RequestParam(name="maxDate", defaultValue = ("")) String maxDate,
+    public Page<Sale> findSales(@RequestParam(name = "minDate", defaultValue = ("")) String minDate,
+                                @RequestParam(name = "maxDate", defaultValue = ("")) String maxDate,
                                 Pageable pageable) {
 
 
         return saleService.fetchAllSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
